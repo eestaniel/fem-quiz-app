8cc91e3d4ecc4f07a413eae04d664e86352325c6
EEstaniel <edwineestaniel93@gmail.com>
2024-07-03 20:18:38 -0700

    refactor: use QuizIcon util

--- src/components/start_menu/StartMenu.tsx
+++ src/components/start_menu/StartMenu.tsx
@@ -1,4 +1,5 @@
 import styles from "./StartMenu.module.css";
+import QuizIconCategory from "../../utils/QuizIconCategory.tsx";
 
 interface StartMenuProps {
   setSelectedQuiz: (quiz: string) => void;
@@ -6,69 +7,58 @@ interface StartMenuProps {
 }
 
 const StartMenu = ({ setSelectedQuiz, isDark }: StartMenuProps) => {
-
   const handleSelectQuiz = (quiz: string) => {
-    console.log(quiz)
+    console.log(quiz);
     setSelectedQuiz(quiz);
-  }
+  };
   return (
-      <div className={styles.container}>
-        <header>
-          <div className={styles.heading}>
-            <h1 className={`${isDark ? 'dark_element_text' : 'light_element_text'}`}>Welcome to the</h1>
-            <h2 className={`${isDark ? 'dark_element_text' : 'light_element_text'}`}>Frontend Quiz!</h2>
-          </div>
-          <p  className={`${isDark ? 'dark_number_header_text' : 'light_number_header_text'}`}>Pick a subject to get started.</p>
-        </header>
-        <section>
-          <div className={`${styles.selection} ${isDark ? 'dark_element_bg' : 'light_element_bg'}`}
-               onClick={() => handleSelectQuiz("HTML")}
+    <div className={styles.container}>
+      <header>
+        <div className={styles.heading}>
+          <h1
+            className={`${isDark ? "dark_element_text" : "light_element_text"}`}
           >
-            <div className={`${styles.img_container} ${styles.html}`}>
-              <img
-                  src="../../../public/assets/images/icon-html.svg"
-                  alt="html icon"
-              />
-            </div>
-            <h3 className={`${isDark ? 'dark_element_text' : 'light_element_text'}`}>HTML</h3>
-          </div>
-          <div className={`${styles.selection} ${isDark ? 'dark_element_bg' : 'light_element_bg'}`}
-               onClick={() => handleSelectQuiz("CSS")}
+            Welcome to the
+          </h1>
+          <h2
+            className={`${isDark ? "dark_element_text" : "light_element_text"}`}
           >
-            <div className={`${styles.img_container} ${styles.css}`}>
-              <img
-                  src="../../../public/assets/images/icon-css.svg"
-                  alt="css icon"
-              />
-            </div>
-            <h3 className={`${isDark ? 'dark_element_text' : 'light_element_text'}`}>CSS</h3>
-          </div>
-          <div
-              className={`${styles.selection} ${isDark ? 'dark_element_bg' : 'light_element_bg'}`}
-              onClick={() => handleSelectQuiz("JavaScript")}
-          >
-            <div className={`${styles.img_container} ${styles.javascript}`}>
-              <img
-                  src="../../../public/assets/images/icon-js.svg"
-                  alt="javascript icon"
-              />
-            </div>
-            <h3 className={`${isDark ? 'dark_element_text' : 'light_element_text'}`}>Javascript</h3>
-          </div>
-          <div
-              className={`${styles.selection} ${isDark ? 'dark_element_bg' : 'light_element_bg'}`}
-              onClick={() => handleSelectQuiz("Accessibility")}
-          >
-            <div className={`${styles.img_container} ${styles.accessibility}`}>
-              <img
-                  src="../../../public/assets/images/icon-accessibility.svg"
-                  alt="accessibility icon"
-              />
-            </div>
-            <h3 className={`${isDark ? 'dark_element_text' : 'light_element_text'}`}>Accessibility</h3>
-          </div>
-        </section>
-      </div>
+            Frontend Quiz!
+          </h2>
+        </div>
+        <p
+          className={`${isDark ? "dark_number_header_text" : "light_number_header_text"}`}
+        >
+          Pick a subject to get started.
+        </p>
+      </header>
+      <section>
+        <div
+          className={`${styles.selection} ${isDark ? "dark_element_bg" : "light_element_bg"}`}
+          onClick={() => handleSelectQuiz("HTML")}
+        >
+          {QuizIconCategory({ isDark, quizType: "HTML" })}
+        </div>
+        <div
+          className={`${styles.selection} ${isDark ? "dark_element_bg" : "light_element_bg"}`}
+          onClick={() => handleSelectQuiz("CSS")}
+        >
+          {QuizIconCategory({ isDark, quizType: "CSS" })}
+        </div>
+        <div
+          className={`${styles.selection} ${isDark ? "dark_element_bg" : "light_element_bg"}`}
+          onClick={() => handleSelectQuiz("JavaScript")}
+        >
+          {QuizIconCategory({ isDark, quizType: "JavaScript" })}
+        </div>
+        <div
+          className={`${styles.selection} ${isDark ? "dark_element_bg" : "light_element_bg"}`}
+          onClick={() => handleSelectQuiz("Accessibility")}
+        >
+          {QuizIconCategory({ isDark, quizType: "Accessibility" })}
+        </div>
+      </section>
+    </div>
   );
 };
 
